f70328a18bc7c97a9874039a69b8a3d91ad9cdda
akhdrzki <akhadirizky@gmail.com>
2024-04-23 09:26:12 +0800

    add (getPelangganByParams, deletePelanggan, updatePelanggan)

--- src/controllers/Pelanggan.js
+++ src/controllers/Pelanggan.js
@@ -33,3 +33,55 @@ export const cPelanggan = async (req, res) => {
     res.status(500).json({ msg: error.message })
   }
 };
+
+export const gPelangganByParams = async (req, res) => {
+  const paramsId = parseInt(req.params.id);
+  try {
+    const pelanggan = await prisma.pelanggan.findFirst({
+      where: {
+        ID_PELANGGAN: paramsId 
+      },
+    });
+    res.status(200).json(pelanggan);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const dPelanggan = async (req, res) => {
+  const params = parseInt(req.params.id);
+
+  try {
+    const pelanggan = await prisma.pelanggan.delete({
+      where: {
+        ID_PELANGGAN: params,
+      },
+    });
+    res.status(200).json({ msg: "data deleted", pelanggan });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const uPelanggan = async (req, res) => {
+  const upPelanggan = req.body;
+  const params = parseInt(req.params.id);
+
+  try {
+    const pelanggan = await prisma.pelanggan.update({
+      where: {
+        ID_PELANGGAN: params,
+      },
+
+      data: {
+        NAMA_PELANGGAN: upPelanggan.NAMA_PELANGGAN,
+        ALAMAT: upPelanggan.ALAMAT,
+        NOMOR_TELEPON: upPelanggan.NOMOR_TELEPON,
+      },
+    });
+    res.status(200).json({ msg: "data updated", data: pelanggan });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
